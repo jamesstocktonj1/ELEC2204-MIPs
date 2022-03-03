d542202fe89705350c569b689ae47da0605bfaba
jamesstocktonj1 <james.stocktonj@gmail.com>
2022-03-03 22:43:35 +0000

    Create register.h

---
+++ register.h
@@ -0,0 +1,19 @@
+#ifndef REGISTER_H
+#define REGISTER_H
+
+#define REGISTER_SIZE 32
+
+
+class Registers {
+
+public:
+    Registers();
+
+    int readRegister(int addr);
+    void writeRegister(int addr, int data);
+
+    ~Registers();
+};
+
+
+#endif
