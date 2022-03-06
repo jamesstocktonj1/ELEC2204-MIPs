4aa5e768e417eea5204a481b80e9609008c531bf
jamesstocktonj1 <james.stocktonj@gmail.com>
2022-03-06 08:51:22 +0000

    Create pc.cpp

---
+++ pc.cpp
@@ -0,0 +1,20 @@
+#include "pc.h"
+
+
+
+
+PC::PC() {
+    programCounter = 0;
+}
+
+void PC::setPC(int value) {
+    programCounter = value;
+}
+
+int PC::getPC() {
+    return programCounter;
+}
+
+void PC::incrementPC() {
+    programCounter += INCREMENT_SIZE;
+}
