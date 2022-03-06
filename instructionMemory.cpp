30a525d943235ffdd263167b45965bd6ff2a25ae
jamesstocktonj1 <james.stocktonj@gmail.com>
2022-03-06 08:50:48 +0000

    Create instructionMemory.cpp

---
+++ instructionMemory.cpp
@@ -0,0 +1,27 @@
+#include "instructionMemory.h"
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+
+InstructionMemory::InstructionMemory() {
+
+    memory = (int *)malloc(INSTRUCTION_MEMORY_SIZE);
+}
+
+InstructionMemory::~InstructionMemory() {
+
+    free(memory);
+}
+
+int InstructionMemory::loadData(int address) {
+
+    return memory[address];
+}
+
+void InstructionMemory::loadFromFile(char *fileName) {
+
+    std::ifstream file;
+    file.open(fileName);
+}
