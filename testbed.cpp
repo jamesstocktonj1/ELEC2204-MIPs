d6e284c38c0304fb671e56a1a6bb355b499799a3
jamesstocktonj1 <james.stocktonj@gmail.com>
2022-03-07 13:36:08 +0000

    Add ALU Test

--- testbed.cpp
+++ testbed.cpp
@@ -4,6 +4,7 @@
 #include "pc.h"
 #include "dataMemory.h"
 #include "register.h"
+#include "alu.h"
 
 
 using namespace std;
@@ -11,7 +12,7 @@ using namespace std;
 void testRegister(void);
 void testMemory(void);
 void testPC(void);
-
+void testALU(void);
 
 int main() {
 
@@ -22,6 +23,7 @@ int main() {
     testRegister();
     testMemory();
     testPC();
+    testALU();
 
     return 0;
 }
@@ -133,6 +135,76 @@ void testPC() {
 
     if(jumpValue != jumpAddr) {
         cout << "Failed to set PC to jump address" << endl;
+        passTest = false;
+    }
+
+    //print whether task passed / failed
+    cout << "Test: " << ((passTest) ? "PASS" : "FAIL") << endl;
+}
+
+
+
+void testALU() {
+
+    cout << "\nALU Test" << endl;
+
+    ALU alu;
+    int valueA = 26;
+    int valueB = 12;
+
+    cout << "Testing Operations:" << endl;
+
+    cout << "AND" << endl;
+    int andResult = alu.performComputation(valueA, valueB, AND_OP);
+
+    cout << "OR" << endl;
+    int orResult = alu.performComputation(valueA, valueB, OR_OP);
+
+    cout << "ADD" << endl;
+    int addResult = alu.performComputation(valueA, valueB, ADD_OP);
+
+    cout << "SUB" << endl;
+    int subResult = alu.performComputation(valueA, valueB, SUB_OP);
+
+    cout << "SLT" << endl;
+    int sltResult = alu.performComputation(valueA, valueB, SLT_OP);
+
+    cout << "NOR" << endl;
+    int norResult = alu.performComputation(valueA, valueB, NOR_OP);
+
+    cout << "ZERO" << endl;
+    int zeroResult = alu.performComputation(valueA, valueA, SUB_OP);
+
+
+    bool passTest = true;
+
+    if(andResult != (valueA & valueB)) {
+        cout << "AND failed" << endl;
+        passTest = false;
+    }
+
+    if(orResult != (valueA | valueB)) {
+        cout << "OR failed" << endl;
+        passTest = false;
+    }
+
+    if(addResult != (valueA + valueB)) {
+        cout << "ADD failed" << endl;
+        passTest = false;
+    }
+
+    if(subResult != (valueA - valueB)) {
+        cout << "SUB failed" << endl;
+        passTest = false;
+    }
+
+    if(sltResult != ((valueA < valueB) ? 0xFF : 0x00)) {
+        cout << "SLT failed" << endl;
+        passTest = false;
+    }
+
+    if(norResult != ~(valueA | valueB)) {
+        cout << "NOR failed" << endl;
         passTest = false;
     }
 
