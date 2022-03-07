382deef20a03029ead3e102f6a762b42cc9ba95f
jamesstocktonj1 <james.stocktonj@gmail.com>
2022-03-07 13:34:15 +0000

    Create ALU

---
+++ alu.cpp
@@ -0,0 +1,52 @@
+#include "alu.h"
+
+
+ALU::ALU() {
+
+}
+
+ALU::~ALU() {
+
+}
+
+int ALU::performComputation(int value1, int value2, int operation) {
+
+    switch(operation) {
+
+        case AND_OP:
+            aluResult = value1 & value2;
+            break;
+        
+        case OR_OP:
+            aluResult = value1 | value2;
+            break;
+
+        case ADD_OP:
+            aluResult = value1 + value2;
+            break;
+        
+        case SUB_OP:
+            aluResult = value1 - value2;
+            break;
+
+        case SLT_OP:
+            aluResult = (value1 < value2) ? 0xFF : 0x00;
+            break;
+
+        case NOR_OP:
+            aluResult =  ~(value1 | value2);
+            break;
+
+        default:
+            return 0;
+            break;
+    }
+
+
+    return aluResult;
+}
+
+int ALU::equalsZero() {
+
+    return (aluResult == 0);
+}
--- alu.h
+++ alu.h
@@ -2,6 +2,20 @@
 #define ALU_H
 
 
+
+//operation constants
+#define AND_OP 0b0000
+#define OR_OP 0b0001
+#define ADD_OP 0b0010
+#define SUB_OP 0b0110
+#define SLT_OP 0b0111
+#define NOR_OP 0b1100
+
+
+/**
+ * @brief 
+ * 
+ */
 class ALU {
 
 public:
@@ -12,9 +26,9 @@ public:
 
     ~ALU();
 
+private:
+    int aluResult;
 };
-
-
 
 
 #endif
