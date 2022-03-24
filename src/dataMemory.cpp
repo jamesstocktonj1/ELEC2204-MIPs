d9a92a332a552ea69a763064b80ed947419fd446
js3g20 <js3g20@soton.ac.uk>
2022-03-24 17:01:54 +0000

    Update dataMemory.cpp

--- src/dataMemory.cpp
+++ src/dataMemory.cpp
@@ -1,13 +1,16 @@
 #include "../include/dataMemory.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <iostream>
 #include <fstream>
 
 
 Memory::Memory() {
 
-    memory = (int *)malloc(MEMORY_SIZE);
+    memory = (int *)malloc(MEMORY_SIZE * sizeof(int));
+    memset(memory, 0, MEMORY_SIZE);
 }
 
 Memory::~Memory() {
@@ -34,10 +37,11 @@ void Memory::dumpFile() {
     memoryFile.open("memoryDump.txt", std::ios::out);
 
     for(int i=0; i<MEMORY_SIZE; i++) {
-        sprintf(temp, "%X", memory[i]);
+        
+        sprintf(temp, "0x%04x", memory[i]);
         memoryFile << temp << " ";
 
-        if((i - 1) % 8 == 0) {
+        if(((i + 1) % 16) == 0) {
             memoryFile << "\n";
         }
     }
