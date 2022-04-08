1bc597eb667298b64f957c8235e265c6e0b2cb0a
js3g20 <js3g20@soton.ac.uk>
2022-04-08 23:51:11 +0100

    Update decoder.cpp

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -7,9 +7,41 @@ Decoder::Decoder() {
     curInstruction = 0;
 }
 
-
+Decoder::~Decoder() {
+    
+}
 
 void Decoder::setCurrentInstruction(int inst) {
 
     curInstruction = inst;
 }
+
+
+int Decoder::getAddress1(void) {
+
+    return 0x00;
+}
+
+int Decoder::getAddress2(void) {
+
+    return 0x00;
+}
+
+int Decoder::getWriteAddress(void) {
+
+    return 0x00;
+}
+
+
+ControlLines Decoder::getControlLines(void) {
+
+    ControlLines temp;
+
+    return temp;
+}
+
+int Decoder::getBranchAddress(void) {
+
+    return 0x00;
+}
+
