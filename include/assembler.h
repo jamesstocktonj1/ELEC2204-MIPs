6da33421a4c0f0ea0bf48dfaeac320e036f35d26
js3g20 <js3g20@soton.ac.uk>
2022-04-20 21:51:45 +0100

    Update assembler.h

--- include/assembler.h
+++ include/assembler.h
@@ -1,10 +1,16 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
+#include <iostream>
+#include <stdio.h>
+
+#include "../include/instructionMemory.h"
 
 
+#define BUFFER_SIZE 32
 
 
-
+void loadFromFile(const char* file, InstructionMemory *mem);
+int parseInstruction(char* inst);
 
 #endif
