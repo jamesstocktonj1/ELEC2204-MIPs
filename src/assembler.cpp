bb574242493fa8e7c509c159262171330c0d1b0e
js3g20 <js3g20@soton.ac.uk>
2022-04-22 15:28:30 +0100

    Update assembler.cpp

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -25,18 +25,52 @@ void loadFromFile(const char* file, InstructionMemory *mem) {
             buf[i] = '\0';
 
             Instruction inst = getInstruction(buf);
+            int curInstruction = 0;
+            std::cout << "Opcode: 0x" << std::hex << getOpcode(inst.opcode) << std::endl;
+            std::cout << "ALU Op 0x" << std::hex << getALU(inst.opcode) << std::endl;
 
-            switch (inst.instructionType)
-            {
+            //begin construct instruction
+            curInstruction = (0x3f & getOpcode(inst.opcode)) << 26;
+
+            switch (inst.instructionType) {
+
             case IType:
-                std::cout << inst.opcode << "\trs: " << inst.rs << "\trt: " << inst.rt << "\tConst: " << inst.constant << std::endl;
+                std::cout << inst.opcode << "\trt: " << inst.rt << "\trs: " << inst.rs << "\tConst: " << inst.constant << std::endl;
+                
+                //rs - [25:21]
+                curInstruction += (0x1f & inst.rs) << 21;
+
+                //rt - [20:16]
+                curInstruction += (0x1f & inst.rt) << 16;
+
+                //constant - [15:0]
+                curInstruction += (0xff & inst.constant);
                 break;
             case JType:
                 std::cout << inst.opcode << "\tJump: " << inst.constant << std::endl;
                 break;
             case RType:
-                std::cout << inst.opcode << "\trs: " << inst.rs << "\trt: " << inst.rt << "\trd: " << inst.rd << std::endl;
+                std::cout << inst.opcode << "\trd: " << inst.rd << "\trs: " << inst.rs << "\trt: " << inst.rt << std::endl;
+                
+                //rs - [25:21]
+                curInstruction += (0x1f & inst.rs) << 21;
+
+                //rt - [20:16]
+                curInstruction += (0x1f & inst.rt) << 16;
+
+                //rd - [15:11]
+                curInstruction += (0x1f & inst.rd) << 11;
+
+                //shamt - [10:6] = 0
+
+                //function - [5:0]
+                curInstruction += (0x1f & getALU(inst.opcode));
+                break;
             }
+
+            std::cout << "Hex Instruction: 0x" << std::hex << curInstruction << std::endl;
+
+            mem->writeMemory(j, curInstruction);
 
             //mem->writeMemory(j, parseInstruction(buf));
             i = 0;
@@ -55,7 +89,7 @@ void loadFromFile(const char* file, InstructionMemory *mem) {
     std::cout << "End of File" << std::endl;
     fclose(f);
 
-    mem->writeMemory(0, 10);
+    //mem->writeMemory(0, 10);
 }
 
 
@@ -85,7 +119,14 @@ Instruction getInstruction(char* inst) {
 
     for(i=0; i<strlen(inst); i++) {
 
-        if(inst[i] == ' ') {
+        //force read last character
+        if(i == (strlen(inst) - 1)) {
+            temp[j] = inst[i];
+            j++;
+            inst[i] = ' ';
+        }
+
+        if(inst[i] == ' ' || i == (strlen(inst) - 1)) {
             temp[j] = '\0';
             j = 0;
 
@@ -95,16 +136,19 @@ Instruction getInstruction(char* inst) {
                 ins.instructionType = getType(ins.opcode);
             }
 
-            //write rs
+            //write rd or constant
             else if(point == 1) {
                 int tempNum = charToInt(temp);
 
-                ins.rs = tempNum;
-                ins.constant = tempNum;
-
                 if(ins.instructionType == JType) {
+                    ins.constant = tempNum;
                     return ins;
-
+                }
+                else if(ins.instructionType == IType) {
+                    ins.rt = tempNum;
+                }
+                else {
+                    ins.rd = tempNum;
                 }
             }
 
@@ -112,15 +156,19 @@ Instruction getInstruction(char* inst) {
             else if(point == 2) {
                 int tempNum = charToInt(temp);
 
-                ins.rt = tempNum;
+                ins.rs = tempNum;
             }
             
             //write rd / immediate
             else if(point == 3) {
                 int tempNum = charToInt(temp);
 
-                ins.constant = tempNum;
-                ins.rd = tempNum;
+                if(ins.instructionType == IType) {
+                    ins.constant = tempNum;
+                }
+                else {
+                    ins.rt = tempNum;
+                }
                 return ins;
             }
             point++;
@@ -128,6 +176,8 @@ Instruction getInstruction(char* inst) {
         else if(inst[i] == ',');
         else if(inst[i] == '\r');
         else if(inst[i] == '\n');
+        else if(inst[i] == '(');
+        else if(inst[i] == ')');
         else {
             temp[j] = inst[i];
             j++;
@@ -153,36 +203,76 @@ int charToInt(char* num) {
 }
 
 InstructionType getType(char* opcode) {
-    std::cout << "Opcode: " << opcode << "\t";
+    //std::cout << "Opcode: " << opcode << "\t";
     
     if(strcmp(opcode, "add") == 0) {
-        std::cout << "R-Type" << std::endl;
+        //std::cout << "R-Type" << std::endl;
         return RType;
     }
     else if(strcmp(opcode, "addi") == 0) {
-        std::cout << "I-Type" << std::endl;
+        //std::cout << "I-Type" << std::endl;
         return IType;
     }
     else if(strcmp(opcode, "slt") == 0) {
-        std::cout << "R-Type" << std::endl;
+        //std::cout << "R-Type" << std::endl;
         return RType;
     }
     else if(strcmp(opcode, "bne") == 0) {
-        std::cout << "I-Type" << std::endl;
+        //std::cout << "I-Type" << std::endl;
         return IType;
     }
     else if(strcmp(opcode, "sw") == 0) {
-        std::cout << "I-Type" << std::endl;
+        //std::cout << "I-Type" << std::endl;
         return IType;
     }
     else {
-        //std::cout << "Error" << std::endl;
+        std::cout << "Error Unrecognised Instruction" << std::endl;
         return RType;
     }
 }
 
+int getOpcode(char* opcode) {
+
+    if(strcmp(opcode, "add") == 0) {
+        return 0x0;
+    }
+    else if(strcmp(opcode, "addi") == 0) {
+        return 0x8;
+    }
+    else if(strcmp(opcode, "slt") == 0) {
+        return 0x0;
+    }
+    else if(strcmp(opcode, "bne") == 0) {
+        return 0x5;
+    }
+    else if(strcmp(opcode, "sw") == 0) {
+        return 0x2b;
+    }
+    else {
+        std::cout << "Error Unrecognised Opcode" << std::endl;
+        return 0;
+    }
+}
+
+int getALU(char* opcode) {
+
+    if(strcmp(opcode, "add") == 0) {
+        return ADD_OP;
+    }
+    else if(strcmp(opcode, "addi") == 0) {
+        return ADD_OP;
+    }
+    else if(strcmp(opcode, "slt") == 0) {
+        return SLT_OP;
+    }
+    else {
+        std::cout << "Error Unrecognised Opcode" << std::endl;
+        return 0;
+    }
+}
 
 
+/*
 void splitLine(char* inst, char* data[]) {
 
     //char data[6][8];
@@ -218,3 +308,4 @@ void splitLine(char* inst, char* data[]) {
 
     //std::cout << data << std::endl;
 }
+*/
